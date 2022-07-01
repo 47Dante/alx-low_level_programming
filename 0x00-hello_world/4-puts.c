60ea4dc66105003f4cedb8f7f9475f321920d231
Olawale <spikyme16@gmail.com>
2022-06-30 19:43:37 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
-#!/bin/bash
-/**
- *  main - entry point
- *
- *  Return: 0 after printing the function
- */
+#include <stdio.h>
+
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,6 @@
-#!/bin/bash
-/**
- *  main - Entry point
- *
- *  Return: Always 0 (Success)
- */
+#include <stdio.h>
 init main(void)
 {
-	printf9"with proper grammer, but the outcome is a piece of art,\n"):
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
