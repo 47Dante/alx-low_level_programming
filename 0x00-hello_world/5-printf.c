9c0b4899a1959c5a0b37eab6a5dec594e8c8dcfe
Olawale <spikyme16@gmail.com>
2022-06-30 18:25:05 -0700

    tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+init main(void)
+{
+	printf9"with proper grammer, but the outcome is a piece of art,\n"):
+	return (0);
+}
