b089162c351ca8f29bdbdf230dbc90dcdb663e2a
Olawale <spikyme16@gmail.com>
2022-07-06 02:58:43 -0700

    q

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * _islower - Short description, single line
+ * @c: contains a value to be compared
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
